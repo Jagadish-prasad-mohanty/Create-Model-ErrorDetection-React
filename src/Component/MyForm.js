d74200e934f8c16349e155ad5e7aacd746fa096a
jagadish123 <mohantyjagadish123@gmail.com>
2021-06-10 10:45:10 +0530

    use Ref

--- src/Component/MyForm.js
+++ src/Component/MyForm.js
@@ -1,27 +1,34 @@
-import React, { useState } from 'react'
+import React, { useState,useRef } from 'react';
 import ErrorModel from '../ErrorModel/ErrorModel';
 import Button from '../UI/Button';
 import Card from './Card';
 import classes from './MyForm.module.css';
 
 function MyForm(props) {
-    const [name,changeName]=useState("");
-    const [age,changeAge]=useState("");
+    const nameRef=useRef();
+    const ageRef=useRef();
+
+    // const [name,changeName]=useState("");
+    // const [age,changeAge]=useState("");
     const [modelToggle,changeModelToggle]=useState(false);
     const [error,setError]=useState({});
 
-    const onChangeHandler=(e,field)=>{
-        e.preventDefault();
-        if (field==="name"){
-            changeName(e.target.value)
-        }
-        else if(field==="age"){
-            changeAge(e.target.value)
-        }
-    }
+    // const onChangeHandler=(e,field)=>{
+    //     e.preventDefault();
+    //     if (field==="name"){
+    //         changeName(e.target.value)
+    //     }
+    //     else if(field==="age"){
+    //         changeAge(e.target.value)
+    //     }
+    // }
     const onSubmitHandler= (e)=>{
         e.preventDefault();
-        if (name.trim().length===0 || age.trim().length===0){
+        nameRef.current.focus();
+        console.log(nameRef.current.value);
+        const nameEntered=nameRef.current.value;
+        const ageEntered=ageRef.current.value;
+        if (nameEntered.trim().length===0 || ageEntered.trim().length===0){
             setError({
                 title:"Invalid Input",
                 message:"Name and Age should't be blank"
@@ -31,19 +38,19 @@ function MyForm(props) {
             })
             return
         }
-        if (+age<1){
+        if (+ageEntered<1){
             setError({
                 title:"Invalid Age",
                 message:"Age can't be <1"
             })
             changeModelToggle(prevState=>{
-                return !prevState
+                return !prevState;
             })
-            return
+            return;
         }
-        props.getList({mname:name,mage:age});
-        changeName("");
-        changeAge("");
+        props.getList({mname:nameEntered,mage:ageEntered});
+        nameRef.current.value="";
+        ageRef.current.value="";
     }
 
     const closeModelHandler=()=>{
@@ -56,14 +63,21 @@ function MyForm(props) {
         <>
         {modelToggle?<ErrorModel modelClose={closeModelHandler} title={error.title} message={error.message}/>:null}
         <Card>
-
             <form onSubmit={onSubmitHandler} className={classes.MyForm}>
                 <div className={classes.MyFormControl}>
 
                     <label>Enter Your Name:</label>
-                    <input onChange={(e)=>onChangeHandler(e,"name")} value={name}/>
+                    <input 
+                    // onChange={(e)=>onChangeHandler(e,"name")} 
+                    // value={name}
+                    ref={nameRef}
+                    />
                     <label>Enter Your Age:</label>
-                    <input onChange={(e)=>onChangeHandler(e,"age")} value={age}/>
+                    <input 
+                    // onChange={(e)=>onChangeHandler(e,"age")} 
+                    // value={age}
+                    ref={ageRef}
+                    />
                 </div>
                 <Button type="submit"> Submit</Button>
             </form>
--- src/ErrorModel/ErrorModel.js
+++ src/ErrorModel/ErrorModel.js
@@ -1,27 +1,41 @@
 import React from 'react'
+import ReactDOM from 'react-dom';
 import Card from '../Component/Card'
 import Button from '../UI/Button';
 import classes from './ErrorModel.module.css';
 
-function ErrorModel(props) {
+const Backdrop =props=>{
     const closeModel=()=>{
         props.modelClose();
     }
+    return <div onClick={closeModel} className={classes.backdrop}/>
+}
+
+const Model = props=>{
+    const closeModel=()=>{
+        props.modelClose();
+    }
+    return <Card className={classes.Model}>
+            <header className={classes.header}>
+                <h2>{props.title}</h2>
+            </header>
+            <div className={classes.content}>
+                <p>{props.message}</p>
+            </div>
+            <footer className={classes.footer}>
+                <Button onClick={closeModel}>Okey</Button>
+            </footer>
+        </Card>
+}
+
+function ErrorModel(props) {
+    
     return (
-        <div >
-        <div onClick={closeModel} className={classes.backdrop}/>
-        <Card className={classes.Model}>
-                <header className={classes.header}>
-                    <h2>{props.title}</h2>
-                </header>
-                <div className={classes.content}>
-                    <p>{props.message}</p>
-                </div>
-                <footer className={classes.footer}>
-                    <Button onClick={closeModel}>Okey</Button>
-                </footer>
-            </Card>
-        </div>
+        <>
+            {ReactDOM.createPortal(<Backdrop onClick={props.onClick} modelClose={props.modelClose}/>,document.getElementById('backdrop-root'))}
+            {ReactDOM.createPortal(<Model title={props.title} message={props.message} modelClose={props.modelClose} onClick={props.onClick}/>,document.getElementById('model-root'))}
+        
+        </>
         
     )
 }
